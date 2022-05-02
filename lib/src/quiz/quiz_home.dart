fed82eb5185c3e665289ceaa886adc5cb7c5b232
Vikas Kumar <mr.rajput7547@gmail.com>
2022-05-02 09:33:06 +0530

    added stagger animation to list

--- lib/src/quiz/quiz_home.dart
+++ lib/src/quiz/quiz_home.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import '../utilities/export.dart';
 
+part 'quiz_category.dart';
 part 'quiz_list_item.dart';
 part 'sliver_app_bar.dart';
-part 'quiz_category.dart';
 
 class QuizHome extends StatefulWidget {
   const QuizHome({Key? key}) : super(key: key);
@@ -15,6 +15,38 @@ class QuizHome extends StatefulWidget {
 
 class _QuizHomeState extends State<QuizHome> {
   final ScrollController _scrollController = ScrollController();
+  final Tween<Offset> _offset = Tween<Offset>(
+    begin: const Offset(1.0, 0),
+    end: Offset.zero,
+  );
+  final GlobalKey<SliverAnimatedListState> _listKey =
+      GlobalKey<SliverAnimatedListState>();
+  final List<Widget> _listItems = <Widget>[];
+  bool _showBackToTop = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(addMoreItemsToList);
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      _addWidgets();
+    });
+  }
+
+  Future<void> _addWidgets({int noOfWidgetsToAdd = 5}) async {
+    late int _start;
+    if (_listItems.isEmpty) {
+      _start = 0;
+    } else {
+      _start = _listItems.length - 1;
+    }
+    final int _end = _start + noOfWidgetsToAdd;
+    for (int i = _start; i < _end; i++) {
+      _listItems.insert(i, const _QuizListItem());
+      await Future<void>.delayed(const Duration(milliseconds: 5));
+      _listKey.currentState!.insertItem(i);
+    }
+  }
 
   @override
   void dispose() {
@@ -35,20 +67,53 @@ class _QuizHomeState extends State<QuizHome> {
           const SliverToBoxAdapter(
             child: _QuizCategory(),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              _buildListItem,
-              childCount: 50,
-            ),
+          SliverAnimatedList(
+            key: _listKey,
+            itemBuilder: (
+              BuildContext context,
+              int index,
+              Animation<double> animation,
+            ) {
+              return SlideTransition(
+                position: animation.drive(_offset),
+                child: _listItems[index],
+              );
+            },
+            initialItemCount: _listItems.length,
           )
         ],
       ),
+      floatingActionButton: _showBackToTop
+          ? FloatingActionButton(
+              onPressed: _scrollToTop,
+              backgroundColor: Colors.blueGrey,
+            )
+          : const SizedBox.shrink(),
     );
   }
 
-  Widget _buildListItem(BuildContext context, int index) {
-    return const _QuizListItem();
+  void addMoreItemsToList() {
+    final ScrollPosition _position = _scrollController.position;
+    if (_position.pixels > _position.maxScrollExtent - 120) {
+      _addWidgets(noOfWidgetsToAdd: 1);
+    }
+
+    if (_scrollController.offset > 500) {
+      setState(() {
+        _showBackToTop = true;
+      });
+    } else {
+      setState(() {
+        _showBackToTop = false;
+      });
+    }
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(
+      0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.linear,
+    );
   }
 }
-
-
