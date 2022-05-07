e82ead31fb5021d2d7878119767db4990c561862
Vikas Kumar <mr.rajput7547@gmail.com>
2022-05-07 19:26:46 +0530

    added comment

--- lib/src/app/app_service.dart
+++ lib/src/app/app_service.dart
@@ -3,7 +3,9 @@ import 'package:quiz_app/src/quiz_home/repository/availability_repo.dart';
 import '../../src/utilities/export.dart';
 
 // This class is used to initialize every required dependency in the app
-// For ex: 1. Shared Preferences 2. Any startup dependencies etc.
+// For ex: 1. Shared Preferences 
+//         2. Any startup dependencies
+//         3. Inject Repositories
 class AppService {
   AppService._();
   factory AppService() {
